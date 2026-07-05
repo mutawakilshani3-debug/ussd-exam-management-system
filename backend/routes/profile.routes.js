f38395c01a6a67beaa1d131eb9ef41093b4dbeaf
mutawakilshani3-debug <mutawakilshani3@gmail.com>
2026-07-05 13:41:10 +0000

    Change uploadProfilePicture to uploadProfilePictureToDb

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const { uploadProfilePicture } = require('../middleware/upload');
+const { uploadProfilePictureToDb } = require('../middleware/upload');
 const controller = require('../controllers/profile.controller');
 
 router.use(protect);
 router.get('/', controller.getProfile);
 router.put('/', controller.updateProfile);
-router.post('/picture', uploadProfilePicture.single('picture'), controller.uploadPicture);
+router.post('/picture', uploadProfilePictureToDb.single('picture'), controller.uploadPicture);
 
 module.exports = router;
