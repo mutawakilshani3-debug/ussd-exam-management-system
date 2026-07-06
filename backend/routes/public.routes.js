a6fdef14f5b621c14bb0107907bbc0ee6b4c86fd
mutawakilshani3-debug <mutawakilshani3@gmail.com>
2026-07-06 10:44:23 +0000

    Update public.routes.js

--- backend/routes/public.routes.js
+++ backend/routes/public.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { checkByCourseCode } = require('../controllers/public.controller');
+const { checkByCourseCode, getAllPublished } = require('../controllers/public.controller');
 
 // No protect/authorize middleware on this router - intentionally public.
 router.get('/check', checkByCourseCode);
+router.get('/timetable', getAllPublished);
 
 module.exports = router;
