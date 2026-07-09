6d0f6b1628e22f7997ca7c8c050767d44d31165a
mutawakilshani3-debug <mutawakilshani3@gmail.com>
2026-07-09 03:33:35 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,7 @@ import BulkUpload from './pages/admin/BulkUpload';
 import Reports from './pages/admin/Reports';
 import SiteSettings from './pages/admin/SiteSettings';
 import GeneralTimetableManager from './pages/admin/GeneralTimetableManager';
+import NationalServiceManager from './pages/admin/NationalServiceManager';
 
 import ExaminerDashboard from './pages/examiner/ExaminerDashboard';
 import MyCourses from './pages/examiner/MyCourses';
@@ -82,6 +83,7 @@ export default function App() {
           <Route path="/admin/reports" element={<ProtectedRoute roles={['admin']}><Reports /></ProtectedRoute>} />
           <Route path="/admin/settings" element={<ProtectedRoute roles={['admin']}><SiteSettings /></ProtectedRoute>} />
           <Route path="/admin/general-timetable" element={<ProtectedRoute roles={['admin']}><GeneralTimetableManager /></ProtectedRoute>} />
+          <Route path="/admin/national-service" element={<ProtectedRoute roles={['admin']}><NationalServiceManager /></ProtectedRoute>} />
 
           {/* Examiner */}
           <Route path="/examiner" element={<ProtectedRoute roles={['examiner']}><ExaminerDashboard /></ProtectedRoute>} />
